0d91dd33ec226fe23eba6ee94c97f0d715a40638
jcmr5720 <camilomr1414@hotmail.com>
2025-08-01 14:46:42 -0500

    c temporalmente la carpeta product_cant_tecnolosys

---
+++ product_cant_tecnolosys/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Cantidad de productos en tecnolosys',
+    'summary': 'Obtener la cantidad de productos que hay en la ubicación Tecnolosys',
+    'version': '1.0',
+    'author': 'Juan José Gamboa Ortiz',
+    'category': 'Tools',
+    'depends': ['sale','base','stock','product'],
+    'data': [
+        'views/product_template_views.xml'
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'GPL-3'
+}
